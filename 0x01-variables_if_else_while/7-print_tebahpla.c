88efb31c6deb10183111ff9e5e9c75b6829759e5
Habtwolde <Habtwolde5@gmail.com>
2022-10-14 11:09:24 -0700

    Task 7: 7-print_tebahpla.c Reverse lowcase alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+ *
+ * Betty: Style Documentation:
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

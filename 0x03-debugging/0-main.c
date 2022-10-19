186786ea8a972a4d5e92cb60a0696dd84019435a
Habtwolde <Habtwolde5@gmail.com>
2022-10-19 09:25:02 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,16 +3,14 @@
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
-*
-* positive_or_negative(int i) - determines if the number is zero or not
 */
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }

e492bad4d0369323c40184f3d0018ebc8e05d0ed
Habtwolde <Habtwolde5@gmail.com>
2022-10-24 02:49:07 -0700

    _putchar is added

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,15 +7,16 @@
  */
 void _puts(char *str)
 {
-	int n;
+	int count = 0;
 
-	for (n = 0; n >= 0; n++)
+	while (count >= 0)
 	{
-		if (str[n] == '\0')
+		if (str[count] == '\0')
 		{
 			_putchar('\n');
 			break;
 		}
-		_putchar(str[n]);
+		_putchar(str[count]);
+		count++;
 	}
 }

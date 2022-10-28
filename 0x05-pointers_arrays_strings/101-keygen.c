b1b25c5764d760371377fd5a31db80f514a427d8
Habtwolde <Habtwolde5@gmail.com>
2022-10-28 00:15:11 -0700

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - check the code for Holberton School students.
- * 
- * Return: Always 0.
+ * main - generates keygen.
+ * Return: 0 Always.
  */
 int main(void)
 {
-char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char password[22];
-int i, n, passw, complem;
+	int r = 0, c = 0;
+	time_t t;
 
-srand(time(0));
-
-for (i = 0; passw < 2772; i++)
-{
-n = rand() % 10;
-password[i] = seed[n];
-passw += password[i];
-}
-complem = 2772 - passw;
-password[i] = complem;
-printf("%s\n",  password);
-return (0);
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
 }

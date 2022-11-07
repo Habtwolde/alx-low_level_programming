69cdb22763128f313e4e660481d09a762e2f6a81
Habtwolde <Habtwolde5@gmail.com>
2022-11-07 02:03:37 -0800

    Task 0: pointer that returns a specific character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *charc;
+	unsigned int i;
+
+	while (size == 0)
+		return (NULL);
+	charc = malloc(sizeof(c) * size);
+
+	if (char == null)
+		return (NULL);
+	while (i < size)
+	{
+		charc[i] = c;
+		i++;
+	}
+	return (charc);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+0x0B. C - malloc, free
+
+| Filename | Description |
+| -------- | ----------- |
+| `0-create_array.c` | Creates an array of chars, and initializes it with a specific char |

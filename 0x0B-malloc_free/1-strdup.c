260c29ea6970321178ebf8b43a8a2826202a01e7
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2022-11-08 15:31:02 +0300

    Task 1: concatenated two strings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,44 +2,42 @@
 #include <stdlib.h>
 
 /**
- * str_concat - contatenates two strings.
- * @s1: string 1
- * @s2: string 2
- * Return: pointer of an array of chars as string
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
-
 char *str_concat(char *s1, char *s2)
 {
-    char *s3;
-    int i, j, k, l;
+	char *strout;
+	unsigned int i, j, k, limit;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    for (i = 0; s1[i] != '\0'; i++)
-        ;
-    for (j = 0; s2[j] != '\0'; j++)
-        ;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    s3 = malloc(sizeof(char) * (i + j + 1));
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-    if (s3 == NULL)
-    {
-        free(s3);
-        return (NULL);
-    }
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-    for (k = 0; k < i; k++)
-        s3[k] = s1[k];
+	strout = malloc(sizeof(char) * (i + j + 1));
 
-    l = j;
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
 
-    for (j = 0; j < l; j++)
-    {
-        s3[k] = s2[j];
-    }
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-    return (s3);
-    
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }

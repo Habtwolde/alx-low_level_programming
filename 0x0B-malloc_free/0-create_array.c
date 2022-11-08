0c91fe9c9d6ac3f9a3dccc33069c848383516842
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2022-11-08 15:15:58 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,11 +20,8 @@ char *create_array(unsigned int size, char c)
 	if (cr == NULL)
 		return (NULL);
 
-	while (i < size)
-		{
-			cr[i] = c;
-			i++;
-		}
+	for (i = 0; i < size; i++)
+		cr[i] = c;
 
 	return (cr);
 }

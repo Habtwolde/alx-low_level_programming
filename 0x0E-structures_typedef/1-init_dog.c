be8b5cf350ea329cbc69069fe024ea08d0332062
Habtwolde5 <Habtwolde5@gmail.com>
2022-11-14 17:57:23 +0000

    Task 1:  function that initialize a variable of type struct dog file is added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,11 @@
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of type struct dog to values passed by the user.
- * @d: struct named dog.
+ * init_dog - initializes a variable of type struct dog.
+ * @d: struct dog.
  * @name: name of the dog.
  * @age: age of the dog.
- * @owner: the name of owner of the dog.
+ * @owner: owner of the dog.
  *
  * Return: no return.
  */
@@ -13,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }

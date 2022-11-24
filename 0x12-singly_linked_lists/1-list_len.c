6ed2802cacb63b5bfce34fbdac07c6f408d75c9f
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2022-11-24 07:12:44 +0300

    Task 1, Task 2, Task3 is committed now

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "list.h"
+#include <stdio.h>  
+#include <stdlib.h>
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the list_t list
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+    size_t i = 0;
+
+    while (h != NULL)
+    {
+        h = h->next;
+        i++;
+    }
+    return (i);
+}

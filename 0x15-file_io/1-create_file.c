898f5a5e7dda30381685cb7e2e3895f6669de5d3
Habtwolde5 <Habtwolde5@gmail.com>
2022-12-05 11:23:55 +0000

    0x15. C - File I/O - task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * create_file - a function that creates a file
- * @filename: the name of file
- * @text_content: NULL terminated string to write to the file
- * Return: 1 on success and -1 if it fails
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int no_letters;
-	int permi;
+	int nletters;
+	int rwr;
 
-	if  (!filename)
+	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
@@ -23,12 +24,15 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (no_letters = 0; text_content[no_letters]; no_letters)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-	permi = write(fd, text_content, no_letters);
 
-	if (permi == -1)
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
+
 	close(fd);
+
 	return (1);
 }
